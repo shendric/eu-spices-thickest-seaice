c1e6a9363308dd720fea98e2e3f5c59cae1206dd
Stefan Hendricks <stefan.hendricks@awi.de>
2018-06-19 20:35:07 +0200

    split in recipe and library

---
+++ thickest_seaice_recipe.py
@@ -0,0 +1,31 @@
+from eu-spices-thickest-seaice import get_l2p_file_list, L2PSITCollection
+
+import sys
+
+def main(l2p_repo):
+    """ Add doc here """
+    
+    # Get a list of thickness profiles
+    l2p_file_list = get_l2p_file_list(l2p_repo)
+    print("%g l2p files found" % len(l2p_file_list))
+
+    # Create a collection of l2p sea ice thickness data
+    l2p_collect = L2PSITCollection(l2p_file_list)
+
+    # Plot the collection
+    create_nh_sit_map(l2p_collect)
+
+
+if __name__ == '__main__':
+
+    # Get the path to the sea ice thickness data repository
+    # with some sanity checks
+    try: 
+        l2p_repo = sys.argv[1]
+    except IndexError:
+        sys.exit("Error: path to local l2p repository required")
+    if not os.path.isdir(l2p_repo):
+        raise ValueError("%s is not a valid directory" % str(l2p_repo))
+
+    # Execute the main program 
+    main(l2p_repo)
